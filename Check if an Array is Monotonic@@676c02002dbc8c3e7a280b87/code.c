1725a935c40cd6ac2eaf6b49d6785e1702db9cec
madhavkansal <madhav1370.becse24@chitkara.edu.in>
2025-03-29 12:59:59 +0530

    Solution to Check if an Array is Monotonic

--- Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -8,18 +8,7 @@ int main(){
     }
     
     int p=0;
-    if(n>1){#include <stdio.h>
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-    }
-    
-    int isAscending = 1; // Flag for ascending order
+    if(n>1){int isAscending = 1; // Flag for ascending order
     int isDescending = 1; // Flag for descending order
 
     // Check if the array is sorted in ascending or descending order
@@ -38,9 +27,6 @@ int main() {
     } else {
         printf("NO\n");
     }
-
-    return 0; // Return statement to indicate successful completion
-}
     else{
         printf("YES");
     }
